477b5952302b173aaaf3255fe298179863006324
festuge <111096304+festuge@users.noreply.github.com>
2023-01-25 02:29:23 +0800

    Update claudiApp.groovy

--- vars/claudiApp.groovy
+++ vars/claudiApp.groovy
@@ -1,25 +1,25 @@
-     pipeline {
-        agent any
-        tools {
-            maven 'maven'
-        }
-        stages {
-            stage("Tools initialization"){
-                steps{
-                    sh 'maven --version'
-                    sh 'java -version'
-                }
-            }
-            stage("checkout code"){
-                steps{
-                    git branch: 'main',
-                    url: "${https://github.com/festuge/main-library.git}"
-                }
-            }
-            stage("to-test-maven"){
-                steps{
-                    sh 'mvn -v'
-                }
-            }
-        }
-    }
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
